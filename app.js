e64b8ca472e0e89cfe578fb3256d7167d76d6bd6
Renata <renata.rodriguesdasilva@outlook.com>
2025-08-06 15:16:09 -0300

    feat: verifica se o botao adicionar amigo, foi clicado

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+function exibirTextoNaTela(tag, texto) {
+  let campo = document.querySelector(tag);
+  campo.innerHTML = texto;
+}
+exibirTextoNaTela("h1", "Amigo Secreto da Maki");
+
+function adicionarAmigo() {
+  console.log("O nome foi adicionado");
+}
+
+let amigos = [];
