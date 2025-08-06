c9c8e79b1ae70bd6ea2cd090027ca8e1f8bcb3aa
Renata <renata.rodriguesdasilva@outlook.com>
2025-08-06 16:44:09 -0300

    feat update

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 //O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
 function exibirTextoNaTela(tag, texto) {
   let campo = document.querySelector(tag);
   campo.innerHTML = texto;
@@ -6,7 +8,27 @@ function exibirTextoNaTela(tag, texto) {
 exibirTextoNaTela("h1", "Amigo Secreto da Maki");
 
 function adicionarAmigo() {
-  console.log("O nome foi adicionado");
+  const nomeDoAmigo = document.getElementById("amigo");
+  amigos.push(nomeDoAmigo.value);
+  nomeDoAmigo.value = "";
+  console.log(`amigos: ${amigos}`);
+  console.log(nomeDoAmigo);
 }
 
-let amigos = [];
+function sortearAmigo() {
+  let numeroSorteado = Math.floor(Math.random() * amigos.length);
+  let amigoSorteado = amigos[numeroSorteado];
+
+  let resultado = document.getElementById("resultado");
+  let sorteado = document.createElement("li");
+
+  sorteado.innerText = amigoSorteado;
+  resultado.appendChild(sorteado);
+  console.log(resultado);
+  amigos.filter((amigo) => amigo !== amigoSorteado);
+}
+
+localStorage.setItem("lista-de-amigos", JSON.stringify(amigos));
+const amigos1 = localStorage.getItem("lista-de-amigos");
+
+let listaAmigos = document.getElementById("listaAmigos");
